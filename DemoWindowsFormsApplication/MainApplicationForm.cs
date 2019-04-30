2eb5b80455af4cf26fdadb5abe24ad1444a911bf
Piet Vredeveld <pvr.werk@gmail.com>
2019-04-30 15:48:23 +0200

    Initial commit demo application

---
+++ DemoWindowsFormsApplication/MainApplicationForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace DemoWindowsFormsApplication
+{
+    public partial class MainApplicationForm : Form
+    {
+        private string apiHostUrl;
+
+        public MainApplicationForm()
+        {
+            InitializeComponent();
+            Cef.Initialize(new CefSettings());
+        }
+
+        private async void GetTokenButtonClick(object sender, EventArgs e)
+        {
+            if (!IsApiHostUrlSet())
+                return;
+
+            var visitor = new TaskStringVisitor();
+            using (var frm = new RetrieveTokenForm($"{apiHostUrl}/token", visitor))
+            {
+                frm.Show(this);
+                var bearer = await visitor.Task;
+                bearerTextBox.Text = bearer;
+            }
+        }
+
+        private bool IsApiHostUrlSet()
+        {
+            apiHostUrl = hostTextBox.Text;
+            if (string.IsNullOrWhiteSpace(apiHostUrl))
+            {
+                MessageBox.Show("API host url is verreist.", "Fout", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            return true;
+        }
+
+        private async void ToevoegenButtonClick(object sender, EventArgs e)
+        {
+            if (!IsApiHostUrlSet())
+                return;
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerTextBox.Text);
+
+            var response = await httpClient.PostAsync($"{apiHostUrl}/api/v1/Energielabels/toevoegen", new StringContent("<EPC></EPC>", Encoding.UTF8, "application/xml"));
+            MessageBox.Show(response.StatusCode.ToString(), "Toevoegen");
+        }
+
+        private async void UitbreidenButtonClick(object sender, EventArgs e)
+        {
+            if (!IsApiHostUrlSet())
+                return;
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerTextBox.Text);
+
+            var response = await httpClient.PostAsync($"{apiHostUrl}/api/v1/Energielabels/uitbreiden", new StringContent("<EPC></EPC>", Encoding.UTF8, "application/xml"));
+            MessageBox.Show(response.StatusCode.ToString(), "Uitbreiden");
+        }
+
+        private async void VervangenButtonClick(object sender, EventArgs e)
+        {
+            if (!IsApiHostUrlSet())
+                return;
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerTextBox.Text);
+
+            var response = await httpClient.PostAsync($"{apiHostUrl}/api/v1/Energielabels/vervangen", new StringContent("<EPC></EPC>", Encoding.UTF8, "application/xml"));
+            MessageBox.Show(response.StatusCode.ToString(), "Vervangen");
+        }
+    }
+}
---
+++ DemoWindowsFormsApplication/RetrieveTokenForm.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace DemoWindowsFormsApplication
+{
+    /// <summary>
+    /// Sample form with an embedded browser, to retrieve a bearer token from the EP-Online token endpoint.
+    /// </summary>
+    public partial class RetrieveTokenForm : Form
+    {
+        private readonly string url;
+        private readonly IStringVisitor visitor;
+        ChromiumWebBrowser browser;
+
+        /// <summary>
+        /// ctor of the form
+        /// </summary>
+        /// <param name="url">url of the EP-Online token endpoint.</param>
+        /// <param name="visitor">Visitor that can be awaited for the bearer token.</param>
+        public RetrieveTokenForm(string url, IStringVisitor visitor)
+        {
+            InitializeComponent();
+            InitBrowser(url);
+            this.url = url;
+            this.visitor = visitor;
+        }
+
+        private void InitBrowser(string url)
+        {
+            browser = new ChromiumWebBrowser(url);
+            browser.LoadingStateChanged += browser_LoadingStateChanged;
+            browser.AddressChanged += BrowserAddressChanged;
+            Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+        }
+
+        /// <summary>
+        /// Event handler that watches the redirects and kicks in when the original url returns.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void BrowserAddressChanged(object sender, AddressChangedEventArgs eventArgs)
+        {
+            if (eventArgs.Address == url)
+            {
+                browser.LoadingStateChanged +=  (s, e) =>
+                {
+                    if (!e.IsLoading)
+                    {
+                        browser.GetMainFrame().GetText(visitor);
+                    }
+                };
+            }
+        }
+
+        /// <summary>
+        /// Quick fix needed to correct the return url as long as TVS doesn't have to correct return url configured.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void browser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (e.IsLoading == false)
+            {
+                if (e.Browser.MainFrame.Url.StartsWith("http://localhost/"))
+                {
+                    var codepos = e.Browser.MainFrame.Url.IndexOf("?code");
+                    var url = $"http://localhost:56000/signin-tvs{e.Browser.MainFrame.Url.Substring(codepos)}";
+                    browser.Load(url);
+                }
+            }
+        }
+    }
+}
